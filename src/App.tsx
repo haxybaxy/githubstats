07a59c89982f33a5b7158e214ecfaf8bc25d1eea
haxy <zaidsaheb4@gmail.com>
2024-12-28 02:15:12 +0300

    good enough light mode

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,23 @@ function App() {
 
   return (
     <ThemeProvider>
-      <div className="min-h-screen w-screen bg-gradient-to-br from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 pb-6 relative overflow-hidden">
+      <div className="min-h-screen w-screen bg-gradient-to-br from-slate-100 to-white dark:from-gray-900 dark:to-gray-800 pb-6 relative overflow-hidden">
         {/* Floating blurred elements */}
         <div className="fixed inset-0 z-0">
-          {/* Blues */}
-          <div className="absolute top-1/4 -left-10 w-96 h-96 bg-sky-200/90 dark:bg-[#58a6ff]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob"></div>
-          <div className="absolute bottom-1/3 left-1/3 w-96 h-96 bg-blue-200/90 dark:bg-[#1f6feb]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-4000"></div>
+          {/* Purple-blues */}
+          <div className="absolute top-1/4 -left-10 w-96 h-96 bg-violet-200/80 dark:bg-[#58a6ff]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob"></div>
+          <div className="absolute bottom-1/3 left-1/3 w-96 h-96 bg-purple-200/80 dark:bg-[#1f6feb]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-4000"></div>
 
-          {/* More Blues & Subtle Teals */}
-          <div className="absolute top-1/3 right-1/3 w-96 h-96 bg-indigo-200/90 dark:bg-[#3fb950]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-2000"></div>
-          <div className="absolute -bottom-32 right-1/4 w-96 h-96 bg-cyan-200/90 dark:bg-[#238636]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-6000"></div>
+          {/* Blues with purple tint */}
+          <div className="absolute top-1/3 right-1/3 w-96 h-96 bg-indigo-200/80 dark:bg-[#3fb950]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-2000"></div>
+          <div className="absolute -bottom-32 right-1/4 w-96 h-96 bg-blue-200/80 dark:bg-[#238636]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-6000"></div>
 
-          {/* Additional blue tones */}
-          <div className="absolute top-2/3 right-1/2 w-96 h-96 bg-blue-200/90 dark:bg-[#2ea043]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-3000"></div>
-          <div className="absolute top-1/2 left-1/4 w-96 h-96 bg-sky-200/90 dark:bg-[#388bfd]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-5000"></div>
+          {/* Additional purple tones */}
+          <div className="absolute top-2/3 right-1/2 w-96 h-96 bg-fuchsia-200/80 dark:bg-[#2ea043]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-3000"></div>
+          <div className="absolute top-1/2 left-1/4 w-96 h-96 bg-violet-200/80 dark:bg-[#388bfd]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-5000"></div>
 
-          {/* New blob on the right */}
-          <div className="absolute top-1/3 -right-10 w-96 h-96 bg-blue-200/90 dark:bg-[#58a6ff]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-1000"></div>
+          {/* Right blob */}
+          <div className="absolute top-1/3 -right-10 w-96 h-96 bg-purple-200/80 dark:bg-[#58a6ff]/20 rounded-full mix-blend-multiply dark:mix-blend-soft-light filter blur-3xl opacity-70 animate-blob animation-delay-1000"></div>
         </div>
 
         {/* Optional subtle noise texture overlay */}
