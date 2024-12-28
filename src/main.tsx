5831d8ea38214a1e358bb1dc79247c94916db64e
haxy <zaidsaheb4@gmail.com>
2024-12-28 15:19:45 +0300

    refreshed docs and removed error

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,20 @@ import { client } from './apollo-client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
-);
+const renderApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root')!);
+  root.render(
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </React.StrictMode>
+  );
+};
+
+// Ensure DOM is loaded before rendering
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', renderApp);
+} else {
+  renderApp();
+}
